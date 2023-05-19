33255176d71a9c93d612de4c41b39afad7f1f192
CopyandPaste-png <l.irsigler64@gmail.com>
2023-05-19 15:56:59 +0200

    added php files

---
+++ php/connection.php
@@ -0,0 +1,12 @@
+<?php
+$host="127.0.0.1";
+$user="s2621933";
+$password="s2621933";
+$database="d2621933";
+
+$con =mysqli_connect($host,$user,$password,$database);
+
+if (mysqli_connect_errno()){
+        echo "failed to connect";
+}
+?>
