4137c2f2dd561f3360bece1ef0ded975503e20b4
Prashan <2436566@students.wits.ac.za>
2023-06-04 20:28:20 +0200

    home page to postItem Screen

--- app/src/main/java/com/example/logintest/HomePage.java
+++ app/src/main/java/com/example/logintest/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.logintest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +11,16 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home);
+
+        Button postButton = findViewById(R.id.postItemButton);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the second screen
+                Intent intent = new Intent(HomePage.this, prodAddScreen.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
