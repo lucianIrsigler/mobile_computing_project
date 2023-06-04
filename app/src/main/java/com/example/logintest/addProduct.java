fb729afb2f43226c7a54b9cccf4f4f0c1c3f7b39
Prashan <2436566@students.wits.ac.za>
2023-06-04 10:54:55 +0200

    activity screen for adding products

--- app/src/main/java/com/example/logintest/addProduct.java
+++ app/src/main/java/com/example/logintest/addProduct.java
@@ -10,5 +10,6 @@ public class addProduct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addproduct);
+
     }
 }
