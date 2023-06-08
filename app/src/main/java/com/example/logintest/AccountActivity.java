3488770a9d435fee6cb16c5cd3398f795d310530
CopyandPaste-png <l.irsigler64@gmail.com>
2023-06-08 18:54:09 +0200

    when in login, you could back back into the app so fixed

--- app/src/main/java/com/example/logintest/AccountActivity.java
+++ app/src/main/java/com/example/logintest/AccountActivity.java
@@ -34,7 +34,21 @@ public class AccountActivity extends AppCompatActivity {
         }
     }
 
-    private void replaceFragment(int id, Fragment fragment,String tag){
+    @Override
+    public void onBackPressed() {
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment mainFragment = manager.findFragmentById(R.id.container);
+
+        if (mainFragment instanceof SignUpAccountDetailsFragment){
+            replaceFragment(R.id.container,new LoginFragment(getSupportFragmentManager()),"login");
+        }else if (mainFragment instanceof SignUpPersonalDetailsFragment){
+            replaceFragment(R.id.container,
+                    new SignUpAccountDetailsFragment(getSupportFragmentManager()),
+                    "signUpAccount");
+        }
+    }
+
+    private void replaceFragment(int id, Fragment fragment, String tag){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(id,fragment,tag);
